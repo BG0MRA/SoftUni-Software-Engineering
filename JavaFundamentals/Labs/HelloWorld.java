9f0d6391cafe35d83393c1d5afaacf110ec0db6c
Bazzalth <martin.angelov02@gmail.com>
2022-09-14 20:39:01 +0300

    added first program

--- JavaFundamentals/Labs/HelloWorld.java
+++ JavaFundamentals/Labs/HelloWorld.java
@@ -1,7 +1,8 @@
-package Labss;
+package Labs;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        System.out.println("Hello world!");
     }
 }
