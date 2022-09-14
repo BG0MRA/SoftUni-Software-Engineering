bac891c57557ff4d9873c4a63902cf873cc6cf77
Bazzalth <martin.angelov02@gmail.com>
2022-09-14 22:04:20 +0300

    updated structure of the Project

--- JavaFundamentals/src/Excercise/E01_BasicSyntax/Hello.java
+++ JavaFundamentals/src/Excercise/E01_BasicSyntax/Hello.java
@@ -1,8 +1,7 @@
-package Labs;
+package E01_BasicSyntax;
 
-public class HelloWorld {
+public class Hello {
     public static void main(String[] args) {
-
         System.out.println("Hello world!");
     }
 }
