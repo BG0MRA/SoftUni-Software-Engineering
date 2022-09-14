a98a5ea9ca7234630dbbd75491673a15a68ff39b
Bazzalth <martin.angelov02@gmail.com>
2022-09-14 22:44:48 +0300

    added Program 01 StudentInformation

---
+++ JavaFundamentals/src/Labs/L01_BasicSyntax/P01StudentInformation.java
@@ -0,0 +1,20 @@
+package L01_BasicSyntax;
+
+import java.util.Scanner;
+
+public class P01StudentInformation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String studentName = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        double avgGrade = Double.parseDouble(scanner.nextLine());
+
+        String resultText = String.format("Name: %s, Age: %d, Grade: %.2f",
+                studentName, age, avgGrade);
+
+        System.out.println(resultText);
+
+        //System.out.printf("Name: %s, Age: %d, Grade: %.2f",studentName, age, avgGrade);
+    }
+}
