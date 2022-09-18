9bd534dff9e79fd7b6a6d68118da9f2389a88cfb
Bazzalth <martin.angelov02@gmail.com>
2022-09-18 18:22:34 +0300

    added new Program Exercise 01 P08TriangeOfNumbers

---
+++ JavaFundamentals/src/Exercises/E01BasicSyntax/P08TriangleofNumbers.java
@@ -0,0 +1,20 @@
+package E01BasicSyntax;
+
+import java.util.Scanner;
+
+public class P08TriangleofNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Write a program that receives a number – n and prints a triangle from 1 to n as in the examples.
+        //n will be in the interval [1...20].
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= number ; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.printf("%d ",i);
+            }
+            System.out.println();
+        }
+    }
+}
