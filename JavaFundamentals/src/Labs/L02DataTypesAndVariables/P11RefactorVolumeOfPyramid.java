397f01b7d749bfb0ac199a33252bde89765085cd
Bazzalth <martin.angelov02@gmail.com>
2022-09-22 17:45:05 +0300

    added new program from L02 P11RefactorVolumeOfPyramid

---
+++ JavaFundamentals/src/Labs/L02DataTypesAndVariables/P11RefactorVolumeOfPyramid.java
@@ -0,0 +1,18 @@
+package L02DataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class P11RefactorVolumeOfPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Length: ");
+        double length = Double.parseDouble(scanner.nextLine());
+        System.out.print("Width: ");
+        double width = Double.parseDouble(scanner.nextLine());
+        System.out.print("Height: ");
+        double hight = Double.parseDouble(scanner.nextLine());
+        double volume = (length * width * hight) / 3;
+        System.out.printf("Pyramid Volume: %.2f", volume);
+    }
+}
