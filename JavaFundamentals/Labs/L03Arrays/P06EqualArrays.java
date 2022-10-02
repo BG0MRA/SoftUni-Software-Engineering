93c2eb7d3bbf005f4da09e213b301aadfe7ff7b9
Martin Angelov <martin.angelov02@gmail.com>
2022-10-02 21:51:00 +0300

    added new program to L03 P06EqualArrays

---
+++ JavaFundamentals/Labs/L03Arrays/P06EqualArrays.java
@@ -0,0 +1,37 @@
+package L03Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P06EqualArrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] firstArr = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int[] secondArr = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int sum = 0;
+        boolean isArrEquals = true;
+
+        for (int i = 0; i < firstArr.length; i++) {
+            sum += firstArr[i];
+            if (firstArr[i] != secondArr[i]) {
+                System.out.printf("Arrays are not identical. Found difference at %d index.", i);
+                isArrEquals = false;
+                break;
+            }
+        }
+
+        if (isArrEquals) {
+            System.out.printf("Arrays are identical. Sum: %d", sum);
+        }
+
+    }
+}
