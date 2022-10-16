d699a5a251f4ddf43b32a6f2c5eee120803e154f
Martin Angelov <martin.angelov02@gmail.com>
2022-10-16 12:10:50 +0300

    added new program to E05 P03HouseParty

---
+++ JavaFundamentals/Exercises/E05Lists/P03HouseParty.java
@@ -0,0 +1,45 @@
+package E05Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class P03HouseParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Write a program that keeps track of guests going to a house party.
+        //On the first input line, you are going to receive how many commands you are going to have.
+        //On the next lines you are going to receive some of the following inputs:
+        //• "{name} is going!"
+        //• "{name} is not going!"
+
+        int guestsNumber = Integer.parseInt(scanner.nextLine());
+
+        List<String> guestNames = new ArrayList<>();
+
+        for (int i = 0; i < guestsNumber; i++) {
+            String lineInput = scanner.nextLine();
+            String[] inputGuests = lineInput.split("\\s+");
+            String name = inputGuests[0];
+            if (inputGuests.length == 3) {
+                if (guestNames.contains(name)) {
+                    System.out.printf("%s is already in the list!\n", name);
+                } else {
+                    guestNames.add(name);
+                }
+
+            } else if (inputGuests.length == 4) {
+                if (guestNames.contains(name)) {
+                    guestNames.remove(name);
+                } else {
+                    System.out.printf("%s is not in the list!\n", name);
+                }
+            }
+        }
+
+        for( String name : guestNames) {
+            System.out.println(name);
+        }
+    }
+}
