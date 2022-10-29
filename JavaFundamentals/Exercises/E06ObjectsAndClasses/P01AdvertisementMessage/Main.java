9615431b83276a34d59e57837614ce82fa8dc3ce
Martin Angelov <martin.angelov02@gmail.com>
2022-10-29 14:46:08 +0300

    added new program to E06 P01AdvertisementMessage

---
+++ JavaFundamentals/Exercises/E06ObjectsAndClasses/P01AdvertisementMessage/Main.java
@@ -0,0 +1,17 @@
+package E06ObjectsAndClasses.P01AdvertisementMessage;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Message message = new Message();
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String output = message.randomMessenge();
+            System.out.println(output);
+        }
+    }
+}
