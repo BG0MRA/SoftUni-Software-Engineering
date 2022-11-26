11347c5bc8d9efe26853c779f5f987a6e41d8e9a
Martin Angelov <martin.angelov02@gmail.com>
2022-11-26 14:50:00 +0200

    progress on BinaryTree task

--- JavaDataStructuresFundamentals/Labs/L03HeapsBTS/src/main/java/implementations/BinaryTree.java
+++ JavaDataStructuresFundamentals/Labs/L03HeapsBTS/src/main/java/implementations/BinaryTree.java
@@ -2,35 +2,65 @@ package implementations;
 
 import interfaces.AbstractBinaryTree;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
 public class BinaryTree<E> implements AbstractBinaryTree<E> {
+    private E key;
+    private BinaryTree<E> leftChild;
+    private BinaryTree<E> rightChild;
+
+    public BinaryTree(E key, BinaryTree<E> leftChild, BinaryTree<E> rightChild) {
+        this.key = key;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
 
     @Override
     public E getKey() {
-        return null;
+        return this.key;
     }
 
     @Override
     public AbstractBinaryTree<E> getLeft() {
-        return null;
+        return this.leftChild;
     }
 
     @Override
     public AbstractBinaryTree<E> getRight() {
-        return null;
+        return this.rightChild;
     }
 
     @Override
     public void setKey(E key) {
-
+        this.key = key;
     }
 
     @Override
     public String asIndentedPreOrder(int indent) {
-        return null;
+        StringBuilder resultBuilder = new StringBuilder();
+        String padding = createPadding(indent) + this.getKey();
+        resultBuilder.append(padding);
+
+        if (this.getLeft() != null) {
+            String preOrder = this.getLeft().asIndentedPreOrder(indent + 2);
+            resultBuilder.append(System.lineSeparator()).append(preOrder);
+        }
+        if (this.getRight() != null) {
+            String preOrder = this.getRight().asIndentedPreOrder(indent + 2);
+            resultBuilder.append(System.lineSeparator()).append(preOrder);
+        }
+
+
+        return resultBuilder.toString();
+    }
+
+    private String createPadding(int indent) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < indent; i++) {
+            builder.append(" ");
+        }
+        return builder.toString();
     }
 
     @Override
