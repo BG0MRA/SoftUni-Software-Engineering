3e515ae0dd22e44c67fd105a344df5ce6be2de0e
Martin Angelov <martin.angelov02@gmail.com>
2022-12-08 21:47:07 +0200

    BarberShop tasks 100/100

--- JavaDataStructuresFundamentals/Exams/prepExam/1. Barber Shop_Skeleton/Barber Shop/src/main/java/barbershopjava/BarberShopImpl.java
+++ JavaDataStructuresFundamentals/Exams/prepExam/1. Barber Shop_Skeleton/Barber Shop/src/main/java/barbershopjava/BarberShopImpl.java
@@ -1,66 +1,140 @@
 package barbershopjava;
 
-import java.util.Collection;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BarberShopImpl implements BarberShop {
 
+    //Map <BarbersName , Barbers objects>
+    private Map<String, Barber> barbersByNames;
+    private Map<String, List<Client>> barbersWithClients;
+    private Map<String, Client> clientsByNames;
+
+
+    public BarberShopImpl() {
+        this.barbersByNames = new HashMap<>();
+        this.barbersWithClients = new HashMap<>();
+        this.clientsByNames = new HashMap<>();
+    }
+
     @Override
     public void addBarber(Barber b) {
+        if (barbersByNames.containsKey(b.name)) {
+            throw new IllegalArgumentException();
+        }
+        //adding barber
+        barbersByNames.put(b.name, b);
+        //for assignClient method: when we add new barber we add it also in barbersClients;
+        barbersWithClients.put(b.name, new ArrayList<>());
+
 
     }
 
     @Override
     public void addClient(Client c) {
+        if (clientsByNames.containsKey(c.name)) {
+            throw new IllegalArgumentException();
+        }
+        clientsByNames.put(c.name, c);
 
     }
 
     @Override
     public boolean exist(Barber b) {
-        return false;
+        return barbersByNames.containsKey(b.name);
     }
 
     @Override
     public boolean exist(Client c) {
-        return false;
+        return clientsByNames.containsKey(c.name);
     }
 
     @Override
     public Collection<Barber> getBarbers() {
-        return null;
+        return barbersByNames.values();
     }
 
     @Override
     public Collection<Client> getClients() {
-        return null;
+        return clientsByNames.values();
     }
 
     @Override
     public void assignClient(Barber b, Client c) {
+        if (!exist(b) || !exist(c)) {
+            throw new IllegalArgumentException();
+        }
 
+        //adding clients barber | client has Barber in his class
+        c.barber = b;
+        barbersWithClients.get(b.name).add(c);
     }
 
     @Override
     public void deleteAllClientsFrom(Barber b) {
+        if (!exist(b)) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Client> clientsToRemove = barbersWithClients.get(b.name);
+        clientsToRemove.clear();
+
 
     }
 
     @Override
     public Collection<Client> getClientsWithNoBarber() {
-        return null;
+        return getClients()
+                .stream()
+                .filter(c -> c.barber == null)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Collection<Barber> getAllBarbersSortedWithClientsCountDesc() {
-        return null;
+        //TODO try Iterating the collection barbersWithClients.get(b.name) (this is List<Client>)
+        return getBarbers()
+                .stream()
+                .sorted((b1, b2) -> {
+                    int firstClients = barbersWithClients.get(b1.name).size();
+                    int secondClients = barbersWithClients.get(b2.name).size();
+
+                    return Integer.compare(secondClients, firstClients);
+
+                })
+                .collect(Collectors.toList());
     }
 
     @Override
     public Collection<Barber> getAllBarbersSortedWithStarsDescendingAndHaircutPriceAsc() {
-        return null;
+        return getBarbers()
+                .stream()
+                .sorted((b1, b2) -> {
+                    int result = Integer.compare(b2.stars, b1.stars);
+
+                    if (result == 0) {
+                        result = Integer.compare(b1.haircutPrice, b2.haircutPrice);
+                    }
+
+                    return result;
+                })
+                .collect(Collectors.toList());
     }
 
     @Override
     public Collection<Client> getClientsSortedByAgeDescAndBarbersStarsDesc() {
-        return null;
+        return barbersWithClients.values()
+                .stream()
+                .flatMap(List::stream)
+                .sorted((c1, c2) -> {
+                    int result = Integer.compare(c2.age, c1.age);
+
+                    if (result == 0) {
+                        result = Integer.compare(c2.barber.stars, c1.barber.stars);
+                    }
+
+                    return result;
+                })
+                .collect(Collectors.toList());
     }
 }
