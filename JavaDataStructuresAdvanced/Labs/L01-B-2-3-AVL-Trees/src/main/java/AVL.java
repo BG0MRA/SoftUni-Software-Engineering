9ea73a74466aa97df68a9b764f86cd20384e87dd
Martin Angelov <martin.angelov02@gmail.com>
2022-12-29 13:00:57 +0200

    AVL Tree Tasks 100/100

--- JavaDataStructuresAdvanced/Labs/L01-B-2-3-AVL-Trees/src/main/java/AVL.java
+++ JavaDataStructuresAdvanced/Labs/L01-B-2-3-AVL-Trees/src/main/java/AVL.java
@@ -43,6 +43,51 @@ public class AVL<T extends Comparable<T>> {
             node.right = this.insert(node.right, item);
         }
 
+        updateHeight(node);
+        node = balance(node);
+
+        return node;
+    }
+
+    private Node<T> rotateRight(Node<T> x) {
+        Node<T> temp = x.left; //y
+        x.left = temp.right;
+        temp.right = x;
+
+        updateHeight(x);
+        updateHeight(temp);
+
+        return temp;
+    }
+
+    private Node<T> rotateLeft(Node<T> x) {
+        Node<T> temp = x.right; //y
+        x.right = temp.left;
+        temp.left = x;
+
+        updateHeight(x);
+        updateHeight(temp);
+
+        return temp;
+
+    }
+
+    private Node<T> balance(Node<T> node) {
+        int balanced = height(node.left) - height(node.right);
+
+        if (balanced > 1) {
+            int childBalance = height(node.left.left) - height(node.left.right);//for double rotation
+            if (childBalance < 0) {
+                node.left = rotateRight(node.left);
+            }
+            return rotateRight(node);
+        } else if (balanced < -1) {
+            int childBalance = height(node.right.left) - height(node.right.right);//for double rotation
+            if (childBalance > 0) {
+                node.right = rotateRight(node.right);
+            }
+            return rotateLeft(node);
+        }
         return node;
     }
 
@@ -60,4 +105,16 @@ public class AVL<T extends Comparable<T>> {
 
         return node;
     }
+
+    private void updateHeight(Node<T> node) {
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+    }
+
+    private int height(Node<T> node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.height;
+    }
 }
+
