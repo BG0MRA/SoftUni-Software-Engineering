9a0dd4dbb6c868ae0efd17e8c04ca54931dc8058
Martin Angelov <martin.angelov02@gmail.com>
2023-01-21 10:18:42 +0200

    update on L04 Data Structures Augmentation

--- JavaDataStructuresAdvanced/Labs/L04DataStructuresAugmentation/src/main/java/Main.java
+++ JavaDataStructuresAdvanced/Labs/L04DataStructuresAugmentation/src/main/java/Main.java
@@ -19,7 +19,7 @@ public class Main {
             executeCommand(shoppingCentre, builder, command, params);
         }
 
-        System.out.println(builder.toString());
+        System.out.print(builder.toString());
     }
 
     private static void executeCommand(ShoppingCentre shoppingCentre, StringBuilder builder, String command, String[] params) {
@@ -28,34 +28,34 @@ public class Main {
                 String name = params[0];
                 double price = Double.parseDouble(params[1]);
                 String producer = params[2];
-                builder.append(shoppingCentre.addProduct(name, price, producer));
+                builder.append(shoppingCentre.addProduct(name, price, producer)).append(System.lineSeparator());
                 break;
             }
             case "DeleteProducts": {
                 if (params.length > 1) {
                     String name = params[0];
                     String producer = params[1];
-                    builder.append(shoppingCentre.delete(name, producer));
+                    builder.append(shoppingCentre.delete(name, producer)).append(System.lineSeparator());
                 } else {
                     String producer = params[0];
-                    builder.append(shoppingCentre.delete(producer));
+                    builder.append(shoppingCentre.delete(producer)).append(System.lineSeparator());
                 }
                 break;
             }
             case "FindProductsByName": {
                 String name = params[0];
-                builder.append(shoppingCentre.findProductsByName(name));
+                builder.append(shoppingCentre.findProductsByName(name)).append(System.lineSeparator());
                 break;
             }
             case "FindProductsByProducer": {
                 String producer = params[0];
-                builder.append(shoppingCentre.findProductsByProducer(producer));
+                builder.append(shoppingCentre.findProductsByProducer(producer)).append(System.lineSeparator());
                 break;
             }
             case "FindProductsByPriceRange":
                 double priceFrom = Double.parseDouble(params[0]);
                 double priceTo = Double.parseDouble(params[1]);
-                builder.append(shoppingCentre.findProductsByPriceRange(priceFrom, priceTo));
+                builder.append(shoppingCentre.findProductsByPriceRange(priceFrom, priceTo)).append(System.lineSeparator());
                 break;
             default:
                 break;
